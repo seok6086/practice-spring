9ba9b71421524af876e0ab551e07df61502781d7
S_0ww <seok6086@kakao.com>
2023-02-02 13:22:20 +0900

    .

---
+++ src/main/java/com/mvcPractice/practicespring/controller/PracticeController.java
@@ -0,0 +1,23 @@
+package com.mvcPractice.practicespring.controller;
+
+
+import org.apache.coyote.Request;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class PracticeController {
+
+    @GetMapping("hello")
+    public String hello (Model model){
+        model.addAttribute("data","hello!!");
+        return "hello";
+    }
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model){
+        model.addAttribute("name",name);
+        return "hello-template";
+    }
+}
